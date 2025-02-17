51f3631236bbe0dcbd6810cf0cc269af79512ab4
J. Graham <polymathy78@gmail..com>
2025-02-16 21:28:10 -0500

    create django project & app

---
+++ profiles/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jefferygraham/Desktop/repos/github.com/udemy-courses/winterbottom/profiles-rest-api/profiles/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
